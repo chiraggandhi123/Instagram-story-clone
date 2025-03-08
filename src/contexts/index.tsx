fb106783678b36e8c2a1c5ca943384c7004e3b77
chirag.gandhi <chirag.gandhi>
2025-03-09 03:01:32 +0530

    fix: run build if ts fails

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,4 +1,5 @@
+import { IAppContext } from "@app/types/interfaces/appContext.interface";
 import { createContext } from "react";
-import { IAppContext } from "./types.interface"
+// import { IAppContext } from "./types.interface"
 
 export const AppContext = createContext<null | IAppContext>(null)
--- src/services/toggleModal.ts
+++ src/services/toggleModal.ts
@@ -1,6 +1,7 @@
+import { IPayload } from "@app/types/interfaces/payload.interface"
 import { toggleOverflowHidden } from "./toggleOverflowHidden"
 import { REDUCER_TYPES } from "@app/reducers/types.enums"
-import { IConfig, IPayload } from "@app/reducers/types.interface"
+import { IConfig } from "@app/types/interfaces/config.interface"
 
 export const toggleModal = (dispatch: React.Dispatch<IPayload>, config?: IConfig) => {
   toggleOverflowHidden("body")
